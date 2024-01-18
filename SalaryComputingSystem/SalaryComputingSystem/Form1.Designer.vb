d886c0203d04c8d096f17e0a8d1e06bf285a5593
EmpreynCh <emprhaimquiambao03@gmail.com>
2024-01-18 21:50:38 +0800

    vbnet project

---
+++ SalaryComputingSystem/SalaryComputingSystem/Form1.Designer.vb
@@ -0,0 +1,550 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TXTNAME = New TextBox()
+        TXTWH = New TextBox()
+        TXTDEP = New TextBox()
+        TXTREGRATE = New TextBox()
+        TXTOVERTIME = New TextBox()
+        TXTOVERRATE = New TextBox()
+        TXTNAME2 = New TextBox()
+        TXTDEP2 = New TextBox()
+        TXTREGRATE2 = New TextBox()
+        TXTWH2 = New TextBox()
+        TXTOVERTIME2 = New TextBox()
+        TXTGROSS = New TextBox()
+        TXTOVERRATE2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        TXTSSS = New TextBox()
+        TXTPHIL = New TextBox()
+        TXTPAGIBIG = New TextBox()
+        Label3 = New Label()
+        Label6 = New Label()
+        Label11 = New Label()
+        Label14 = New Label()
+        TXTINTAX = New TextBox()
+        TXTTOTALDEC = New TextBox()
+        TXTNET = New TextBox()
+        Label18 = New Label()
+        Label19 = New Label()
+        Label20 = New Label()
+        Label21 = New Label()
+        BTNCLEAR = New Button()
+        BTNCOM = New Button()
+        SuspendLayout()
+        ' 
+        ' TXTNAME
+        ' 
+        TXTNAME.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTNAME.Location = New Point(191, 67)
+        TXTNAME.Name = "TXTNAME"
+        TXTNAME.Size = New Size(310, 43)
+        TXTNAME.TabIndex = 0
+        ' 
+        ' TXTWH
+        ' 
+        TXTWH.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTWH.Location = New Point(191, 209)
+        TXTWH.Name = "TXTWH"
+        TXTWH.Size = New Size(310, 43)
+        TXTWH.TabIndex = 0
+        ' 
+        ' TXTDEP
+        ' 
+        TXTDEP.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTDEP.Location = New Point(191, 136)
+        TXTDEP.Name = "TXTDEP"
+        TXTDEP.Size = New Size(310, 43)
+        TXTDEP.TabIndex = 0
+        ' 
+        ' TXTREGRATE
+        ' 
+        TXTREGRATE.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTREGRATE.Location = New Point(191, 290)
+        TXTREGRATE.Name = "TXTREGRATE"
+        TXTREGRATE.Size = New Size(310, 43)
+        TXTREGRATE.TabIndex = 0
+        ' 
+        ' TXTOVERTIME
+        ' 
+        TXTOVERTIME.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTOVERTIME.Location = New Point(191, 368)
+        TXTOVERTIME.Name = "TXTOVERTIME"
+        TXTOVERTIME.Size = New Size(310, 43)
+        TXTOVERTIME.TabIndex = 0
+        ' 
+        ' TXTOVERRATE
+        ' 
+        TXTOVERRATE.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTOVERRATE.Location = New Point(191, 443)
+        TXTOVERRATE.Name = "TXTOVERRATE"
+        TXTOVERRATE.Size = New Size(310, 43)
+        TXTOVERRATE.TabIndex = 0
+        ' 
+        ' TXTNAME2
+        ' 
+        TXTNAME2.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTNAME2.Location = New Point(889, 71)
+        TXTNAME2.Name = "TXTNAME2"
+        TXTNAME2.Size = New Size(310, 43)
+        TXTNAME2.TabIndex = 0
+        ' 
+        ' TXTDEP2
+        ' 
+        TXTDEP2.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTDEP2.Location = New Point(889, 136)
+        TXTDEP2.Name = "TXTDEP2"
+        TXTDEP2.Size = New Size(310, 43)
+        TXTDEP2.TabIndex = 0
+        ' 
+        ' TXTREGRATE2
+        ' 
+        TXTREGRATE2.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTREGRATE2.Location = New Point(889, 290)
+        TXTREGRATE2.Name = "TXTREGRATE2"
+        TXTREGRATE2.Size = New Size(310, 43)
+        TXTREGRATE2.TabIndex = 0
+        ' 
+        ' TXTWH2
+        ' 
+        TXTWH2.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTWH2.Location = New Point(889, 209)
+        TXTWH2.Name = "TXTWH2"
+        TXTWH2.Size = New Size(310, 43)
+        TXTWH2.TabIndex = 0
+        ' 
+        ' TXTOVERTIME2
+        ' 
+        TXTOVERTIME2.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTOVERTIME2.Location = New Point(889, 368)
+        TXTOVERTIME2.Name = "TXTOVERTIME2"
+        TXTOVERTIME2.Size = New Size(310, 43)
+        TXTOVERTIME2.TabIndex = 0
+        ' 
+        ' TXTGROSS
+        ' 
+        TXTGROSS.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTGROSS.Location = New Point(889, 518)
+        TXTGROSS.Name = "TXTGROSS"
+        TXTGROSS.Size = New Size(310, 43)
+        TXTGROSS.TabIndex = 0
+        ' 
+        ' TXTOVERRATE2
+        ' 
+        TXTOVERRATE2.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTOVERRATE2.Location = New Point(889, 443)
+        TXTOVERRATE2.Name = "TXTOVERRATE2"
+        TXTOVERRATE2.Size = New Size(310, 43)
+        TXTOVERRATE2.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(17, 75)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(77, 31)
+        Label1.TabIndex = 1
+        Label1.Text = "Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(12, 144)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(144, 31)
+        Label2.TabIndex = 1
+        Label2.Text = "Department"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(12, 290)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(150, 31)
+        Label4.TabIndex = 1
+        Label4.Text = "Regular Rate"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(12, 217)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(178, 31)
+        Label5.TabIndex = 1
+        Label5.Text = "Working Hours"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(12, 368)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(123, 31)
+        Label7.TabIndex = 1
+        Label7.Text = "Over Time"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(17, 443)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(118, 31)
+        Label8.TabIndex = 1
+        Label8.Text = "Over Rate"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label9.Location = New Point(759, 83)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(83, 31)
+        Label9.TabIndex = 1
+        Label9.Text = "Name "
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label10.Location = New Point(711, 148)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(144, 31)
+        Label10.TabIndex = 1
+        Label10.Text = "Department"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label12.Location = New Point(677, 221)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(178, 31)
+        Label12.TabIndex = 1
+        Label12.Text = "Working Hours"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label13.Location = New Point(741, 380)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(114, 31)
+        Label13.TabIndex = 1
+        Label13.Text = "Overtime"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label15.Location = New Point(705, 302)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(150, 31)
+        Label15.TabIndex = 1
+        Label15.Text = "Regular Rate"
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label16.Location = New Point(731, 451)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(124, 31)
+        Label16.TabIndex = 1
+        Label16.Text = "Over Rate:"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label17.Location = New Point(161, 674)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(128, 31)
+        Label17.TabIndex = 1
+        Label17.Text = "PhilHealth"
+        ' 
+        ' TXTSSS
+        ' 
+        TXTSSS.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTSSS.Location = New Point(299, 606)
+        TXTSSS.Name = "TXTSSS"
+        TXTSSS.Size = New Size(244, 43)
+        TXTSSS.TabIndex = 0
+        ' 
+        ' TXTPHIL
+        ' 
+        TXTPHIL.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTPHIL.Location = New Point(299, 666)
+        TXTPHIL.Name = "TXTPHIL"
+        TXTPHIL.Size = New Size(244, 43)
+        TXTPHIL.TabIndex = 0
+        ' 
+        ' TXTPAGIBIG
+        ' 
+        TXTPAGIBIG.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTPAGIBIG.Location = New Point(299, 722)
+        TXTPAGIBIG.Name = "TXTPAGIBIG"
+        TXTPAGIBIG.Size = New Size(244, 43)
+        TXTPAGIBIG.TabIndex = 0
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(185, 730)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(104, 31)
+        Label3.TabIndex = 1
+        Label3.Text = "Pag-ibig"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(739, 518)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(116, 31)
+        Label6.TabIndex = 1
+        Label6.Text = "Gross Pay"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label11.Location = New Point(204, 614)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(53, 31)
+        Label11.TabIndex = 1
+        Label11.Text = "SSS"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label14.Location = New Point(332, 554)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(142, 31)
+        Label14.TabIndex = 2
+        Label14.Text = "Deductions:"
+        ' 
+        ' TXTINTAX
+        ' 
+        TXTINTAX.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTINTAX.Location = New Point(750, 606)
+        TXTINTAX.Name = "TXTINTAX"
+        TXTINTAX.Size = New Size(244, 43)
+        TXTINTAX.TabIndex = 0
+        ' 
+        ' TXTTOTALDEC
+        ' 
+        TXTTOTALDEC.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTTOTALDEC.Location = New Point(750, 662)
+        TXTTOTALDEC.Name = "TXTTOTALDEC"
+        TXTTOTALDEC.Size = New Size(244, 43)
+        TXTTOTALDEC.TabIndex = 0
+        ' 
+        ' TXTNET
+        ' 
+        TXTNET.Font = New Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TXTNET.Location = New Point(759, 718)
+        TXTNET.Name = "TXTNET"
+        TXTNET.Size = New Size(244, 43)
+        TXTNET.TabIndex = 0
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label18.Location = New Point(600, 614)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(135, 31)
+        Label18.TabIndex = 1
+        Label18.Text = "Income Tax"
+        ' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label19.Location = New Point(549, 670)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(195, 31)
+        Label19.TabIndex = 1
+        Label19.Text = "Total Deductions"
+        ' 
+        ' Label20
+        ' 
+        Label20.AutoSize = True
+        Label20.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label20.Location = New Point(633, 722)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(91, 31)
+        Label20.TabIndex = 1
+        Label20.Text = "Netpay"
+        ' 
+        ' Label21
+        ' 
+        Label21.AutoSize = True
+        Label21.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label21.Location = New Point(479, 9)
+        Label21.Name = "Label21"
+        Label21.Size = New Size(310, 31)
+        Label21.TabIndex = 1
+        Label21.Text = "Salary Computation System"
+        ' 
+        ' BTNCLEAR
+        ' 
+        BTNCLEAR.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        BTNCLEAR.Location = New Point(1114, 597)
+        BTNCLEAR.Name = "BTNCLEAR"
+        BTNCLEAR.Size = New Size(181, 72)
+        BTNCLEAR.TabIndex = 3
+        BTNCLEAR.Text = "CLEAR"
+        BTNCLEAR.UseVisualStyleBackColor = True
+        ' 
+        ' BTNCOM
+        ' 
+        BTNCOM.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        BTNCOM.ForeColor = SystemColors.ControlText
+        BTNCOM.Location = New Point(1114, 681)
+        BTNCOM.Name = "BTNCOM"
+        BTNCOM.Size = New Size(181, 72)
+        BTNCOM.TabIndex = 3
+        BTNCOM.Text = "COMPUTE"
+        BTNCOM.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        BackgroundImageLayout = ImageLayout.None
+        ClientSize = New Size(1358, 829)
+        Controls.Add(BTNCOM)
+        Controls.Add(BTNCLEAR)
+        Controls.Add(Label14)
+        Controls.Add(Label20)
+        Controls.Add(Label19)
+        Controls.Add(Label18)
+        Controls.Add(Label11)
+        Controls.Add(Label3)
+        Controls.Add(Label6)
+        Controls.Add(Label17)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label8)
+        Controls.Add(Label13)
+        Controls.Add(Label4)
+        Controls.Add(Label12)
+        Controls.Add(Label7)
+        Controls.Add(Label5)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label2)
+        Controls.Add(Label21)
+        Controls.Add(Label1)
+        Controls.Add(TXTPHIL)
+        Controls.Add(TXTNET)
+        Controls.Add(TXTTOTALDEC)
+        Controls.Add(TXTINTAX)
+        Controls.Add(TXTSSS)
+        Controls.Add(TXTPAGIBIG)
+        Controls.Add(TXTGROSS)
+        Controls.Add(TXTOVERRATE2)
+        Controls.Add(TXTOVERRATE)
+        Controls.Add(TXTOVERTIME2)
+        Controls.Add(TXTOVERTIME)
+        Controls.Add(TXTWH2)
+        Controls.Add(TXTDEP2)
+        Controls.Add(TXTDEP)
+        Controls.Add(TXTREGRATE2)
+        Controls.Add(TXTREGRATE)
+        Controls.Add(TXTWH)
+        Controls.Add(TXTNAME2)
+        Controls.Add(TXTNAME)
+        ForeColor = SystemColors.ControlText
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TXTNAME As TextBox
+    Friend WithEvents TXTWH As TextBox
+    Friend WithEvents TXTDEP As TextBox
+    Friend WithEvents TXTREGRATE As TextBox
+    Friend WithEvents TXTOVERTIME As TextBox
+    Friend WithEvents TXTOVERRATE As TextBox
+    Friend WithEvents TXTNAME2 As TextBox
+    Friend WithEvents TXTDEP2 As TextBox
+    Friend WithEvents TXTREGRATE2 As TextBox
+    Friend WithEvents TXTWH2 As TextBox
+    Friend WithEvents TXTOVERTIME2 As TextBox
+    Friend WithEvents TXTGROSS As TextBox
+    Friend WithEvents TXTOVERRATE2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents TXTSSS As TextBox
+    Friend WithEvents TXTPHIL As TextBox
+    Friend WithEvents TXTPAGIBIG As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents TXTINTAX As TextBox
+    Friend WithEvents TXTTOTALDEC As TextBox
+    Friend WithEvents TXTNET As TextBox
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents BTNCLEAR As Button
+    Friend WithEvents BTNCOM As Button
+End Class
---
+++ SalaryComputingSystem/SalaryComputingSystem/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+    Private Sub BTNCLEAR_Click(sender As Object, e As EventArgs) Handles BTNCLEAR.Click
+
+        TXTNAME.Text = ""
+        TXTDEP.Text = ""
+        TXTWH.Text = ""
+        TXTREGRATE.Text = ""
+        TXTOVERTIME.Text = ""
+        TXTOVERRATE.Text = ""
+        TXTNAME2.Text = ""
+        TXTDEP2.Text = ""
+        TXTWH2.Text = ""
+        TXTREGRATE2.Text = ""
+        TXTOVERTIME2.Text = ""
+        TXTOVERRATE2.Text = ""
+        TXTGROSS.Text = ""
+        TXTSSS.Text = ""
+        TXTPHIL.Text = ""
+        TXTPAGIBIG.Text = ""
+        TXTINTAX.Text = ""
+        TXTTOTALDEC.Text = ""
+        TXTNET.Text = ""
+
+    End Sub
+    Private Sub BTNCOM_Click(sender As Object, e As EventArgs) Handles BTNCOM.Click
+        Dim NAME = TXTNAME.Text
+        Dim DEP = TXTDEP.Text
+        Dim WH = TXTWH.Text
+        Dim REGRATE = TXTREGRATE.Text
+        Dim TIME = TXTOVERTIME.Text
+        Dim RATE = TXTOVERRATE.Text
+        Dim SSS As Double = 0.05
+        Dim PHIL As Double = 150
+        Dim IBIG As Double = 0.03
+        Dim INCOMERATE As Double = 0.08
+        Dim REGPAY As Double = WH * REGRATE
+        Dim PAY As Double = TIME * RATE
+        Dim GPAY = REGPAY + PAY
+        Dim SSSDED As Double = GPAY * SSS
+        Dim PHILDED As Double = PHIL
+        Dim IBIGDED As Double = GPAY * IBIG
+        Dim TAX As Double = GPAY - (SSSDED + PHILDED + IBIGDED)
+        Dim TAXDED = TAX * INCOMERATE
+        Dim TOTALDED = SSSDED + PHIL + IBIGDED + TAXDED
+        Dim NETPAY As Double = GPAY - TOTALDED
+        TXTNAME2.Text = NAME
+        TXTDEP2.Text = DEP
+        TXTWH2.Text = WH
+        TXTREGRATE2.Text = REGRATE
+        TXTOVERTIME2.Text = TIME
+        TXTOVERRATE2.Text = RATE
+        TXTGROSS.Text = GPAY.ToString("0.00")
+        TXTSSS.Text = SSSDED.ToString("0.00")
+        TXTPHIL.Text = PHILDED.ToString("0.00")
+        TXTPAGIBIG.Text = IBIGDED.ToString("0.00")
+        TXTINTAX.Text = TAXDED.ToString("0.00")
+        TXTTOTALDEC.Text = TOTALDED.ToString("0.00")
+        TXTNET.Text = NETPAY.ToString("0.00")
+
+    End Sub
+End Class
